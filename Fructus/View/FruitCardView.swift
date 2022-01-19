09d5934b3d804eb2049c226adaedab8878fd9a67
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-19 00:32:11 -0500

    Adding the fruit cardView with all their information as the image, tittle, description and the button for next actions

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,67 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/18/22.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    
+    //MARK: - PROPERTIES
+    
+    @State private var isAnimating: Bool = false
+    
+    //MARK: - BODY
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6) // Animation
+                
+                // FRUIT: TITLE
+                Text("Blueberry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                // FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutritious and wildly popular fruit all over the world.")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                // BUTTON: START
+                StartButtonView()
+            }//: VSTACK
+        }//: ZSTACK
+        .onAppear(perform: {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        })
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(
+            gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]),
+            startPoint: .top,
+            endPoint: .bottom))
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
+    }
+}
+
+//MARK: - PREVIEW
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 340, height: 640))
+    }
+}
---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,45 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/19/22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    var body: some View {
+        Button {
+            print("Exit the onboarding")
+        } label: {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+                    
+            } //: HSTACK
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+            )
+        } //: BUTTON
+        .tint(.white)
+
+    }
+}
+
+//MARK: - PREVIEW
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
