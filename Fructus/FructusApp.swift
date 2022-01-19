cf7e86680b506fd7f0b1769ad4621b646769ca12
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-18 23:37:41 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
